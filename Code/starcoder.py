eb25ad89befa74991264e26a3f8968cdb405a4ec
Sona James <112008266+SonaJames12@users.noreply.github.com>
2025-10-15 19:55:16 -0400

    Starcoder2-7B model
    Still needs work

---
+++ Code/starcoder.py
@@ -0,0 +1,13 @@
+# pip install git+https://github.com/huggingface/transformers.git # TODO: merge PR to main
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+checkpoint = "bigcode/starcoder2-7b"
+device = "cuda" # for GPU usage or "cpu" for CPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# for multiple GPUs install accelerate and do `model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto")`
+model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
+
+inputs = tokenizer.encode("def add_two_numbers():", return_tensors="pt").to(device)
+outputs = model.generate(inputs,max_new_tokens=150,do_sample=False)
+print(tokenizer.decode(outputs[0]))
